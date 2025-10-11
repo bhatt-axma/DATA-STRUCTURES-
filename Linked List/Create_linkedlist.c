e532d20c649a7c25cc518c01de6c7865c7e9ac91
Asma Jan <bhattisha3115@gmail.com>
2025-10-11 11:43:04 +0530

    Update Create_linkedlist.c

--- Linked List/Create_linkedlist.c
+++ Linked List/Create_linkedlist.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node*next;
+};
+int main(){
+struct node*head=(struct node*)malloc(sizeof(struct node));
+struct node*second=(struct node*)malloc(sizeof(struct node));
+struct node*third=(struct node*)malloc(sizeof(struct node));
 
+head->data=10;
+head->next=second;
+second->data=30;
+second->next=third;
+third->data=78;
+third->next=NULL;
+struct node*temp=head;
+while(temp!=NULL){
+printf("%d->",temp->data);
+temp=temp->next;
+}
+printf("NULL\n");
+return 0;
+}
